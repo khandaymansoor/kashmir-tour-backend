ad12c68f00df476fa886516a5e61ca62b07eccea
Mansoor Khanday <mansoorkhanday@Mansoors-MacBook-Air.local>
2026-01-02 21:57:07 +0530

    Fix ESM export for MySQL connection

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2");
+import mysql from "mysql2";
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -17,4 +17,4 @@ connection.connect((err) => {
   }
 });
 
-module.exports = connection;
+export default connection;
